4fec37620de08c1eef7f4c168f1ecf0d96e05df7
Teembokeem <myungtkim@gmail.com>
2016-10-29 18:16:20 -0700

    adds template content for slides

--- www/views/home/home-carousel/Component.home-carousel.js
+++ www/views/home/home-carousel/Component.home-carousel.js
@@ -17,6 +17,35 @@
 
         cc.$onInit = function () {
             $log.instantiate('Home Carousel', 'Component Controller');
+
+            cc.slideContent = [
+                {
+                    title: 'INNSBRUCK-TIROL TICKETS',
+                    content: 'ON SALE NOW',
+                    buttons: [
+                        {
+                            label: 'LEARN MORE',
+                            dest: 'app.here'
+                        },
+                        {
+                            label: 'BUY NOW',
+                            dest: 'app.there'
+                        }
+                    ],
+                    image: 'img/slide1.jpeg'
+                },
+                {
+                    title: 'AIR + STYLE LOS ANGELES FEBRUARY 18 + 19',
+                    content: 'SIGN UP FOR PRESALE TICKETS',
+                    buttons: [
+                        {
+                            label: '+ SIGN UP +',
+                            dest: 'app.another'
+                        }
+                    ],
+                    image: 'img/slide2.jpeg'
+                }
+            ]
         }
     }
 })();
