fca9ef207d5847b2445d35b29bddeaababa419b5
Teembokeem <myungtkim@gmail.com>
2016-10-30 03:49:49 -0700

    adds schedule componet for home page

---
+++ www/views/home/home-schedule/Component.home-schedule.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+    angular.module('Home.view.module')
+        .component('homeSchedule', {
+            bindings: {
+
+            },
+            templateUrl: 'views/home/home-schedule/home-schedule.html',
+            controller: homeScheduleCtrl,
+            controllerAs: 'cc'
+        })
+
+    homeScheduleCtrl.$inject = ['$log'];
+
+    function homeScheduleCtrl($log) {
+        var cc = this;
+
+        cc.$onInit = function () {
+            $log.instantiate('Home Schedule', 'Component Controller');
+
+            cc.scheduleContent = [
+                {
+                    title: 'BEIJING',
+                    date: 'NOV 18 + 19',
+                    dest: 'app.here'
+                },
+                {
+                    title: 'INNSBRUCK',
+                    date: 'FEB 3 + 4',
+                    dest: 'app.there'
+                },
+                {
+                    title: 'LOS ANGELES',
+                    date: 'FEB 10 + 14',
+                    dest: 'app.anywhere'
+                },
+            ]
+        }
+    }
+})();
