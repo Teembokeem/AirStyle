0a0c4fb3f7422af2a3a09ca9b2c75e7898da1b0f
Teembokeem <myungtkim@gmail.com>
2016-10-30 18:37:36 -0700

    adds home footer component with boiler html

---
+++ www/views/home/home-footer/Component.home-footer.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+    angular.module('Home.view.module')
+        .component('homeFooter', {
+            bindings: {
+
+            },
+            templateUrl: 'views/home/home-footer/home-footer.html',
+            controller: HomeFooterCtrl,
+            controllerAs: 'cc'
+        })
+
+    HomeFooterCtrl.$inject = ['$log'];
+
+    function HomeFooterCtrl($log) {
+        var cc = this;
+
+        cc.$onInit = function () {
+            $log.instantiate('Home Footer', 'Component Controller');
+        }
+    }
+})();
