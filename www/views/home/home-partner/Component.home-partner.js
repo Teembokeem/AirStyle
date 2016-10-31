31b1e88326bd362bfde2c5f4d355f339c0514f52
Teembokeem <myungtkim@gmail.com>
2016-10-30 18:37:46 -0700

    adds home partner component with boiler html

---
+++ www/views/home/home-partner/Component.home-partner.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+    angular.module('Home.view.module')
+        .component('homePartner', {
+            bindings: {
+
+            },
+            templateUrl: 'views/home/home-partner/home-partner.html',
+            controller: HomePartnerCtrl,
+            controllerAs: 'cc'
+        })
+
+    HomePartnerCtrl.$inject = ['$log'];
+
+    function HomePartnerCtrl($log) {
+        var cc = this;
+
+        cc.$onInit = function () {
+            $log.instantiate('Home Partner', 'Component Controller');
+
+            cc.partnerContent = [
+                'img/partner-1.png',
+                'img/partner-2.png',
+                'img/partner-3.png',
+                'img/partner-4.png',
+                'img/partner-5.png',
+                'img/partner-6.png',
+            ]
+        }
+    }
+})();
