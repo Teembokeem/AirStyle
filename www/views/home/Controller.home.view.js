fd306664f09fd5e3c2338328e6ce506256c4d316
Greg Pynes <gpynes@gmail.com>
2016-11-02 22:35:07 -0700

    adds nav to hamburger on scroll

--- www/views/home/Controller.home.view.js
+++ www/views/home/Controller.home.view.js
@@ -5,9 +5,19 @@
         .module('Home.view.module')
         .controller('HomeCtrl', HomeCtrl);
 
-    HomeCtrl.$inject = ['$log', '$state'];
+    HomeCtrl.$inject = ['$log', '$state', '$rootScope', '$ionicScrollDelegate', '$window'];
 
-    function HomeCtrl($log, $state) {
+    function HomeCtrl($log, $state, $rootScope, $ionicScrollDelegate, $window) {
         var cc = this;
+        var t = $ionicScrollDelegate.$getByHandle('mainScroll');
+        cc.getScroll = function () {
+            var fromTop = t.getScrollPosition().top;
+            if (fromTop > 50) {
+                $rootScope.$broadcast('noNav', true);
+            } else {
+                $rootScope.$broadcast('noNav', false);
+            }
+
+        }
     }
 })();
