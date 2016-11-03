b66e29c241216b7aff1fde3876b100f9c11f6288
Greg Pynes <gpynes@gmail.com>
2016-11-03 01:03:27 -0700

    adds mobile-menu component

---
+++ www/views/global-components/mobile-menu/mobile-menu.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+    angular.module('Template')
+        .component('mobileMenu', {
+            templateUrl: 'views/global-components/mobile-menu/mobile-menu.html',
+            controller: MobileMenuCtrl,
+            controllerAs: 'cc'
+        })
+
+    MobileMenuCtrl.$inject = ['$log'];
+
+    function MobileMenuCtrl($log) {
+        var cc = this;
+
+        cc.toggleNav = function () {
+            cc.noNav = !cc.noNav;
+        };
+
+        cc.$onInit = function () {
+            cc.events = [
+                {
+                    name: 'Beijing',
+                    color: '#62ff00',
+                    date: 'NOV 18 + 19'
+                },
+                {
+                    name: 'INNSBRUCK',
+                    color: '#f9fe17',
+                    date: 'FEB 3 + 4'
+                },
+                {
+                    name: 'LOS ANGELES',
+                    color: '#40caff',
+                    date: 'FEB 10 + 14'
+                }
+            ];
+        }
+    }
+})();
