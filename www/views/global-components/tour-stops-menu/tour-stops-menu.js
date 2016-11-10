58634ae10e1538b85fc2074091fa094701e08114
Teembokeem <myungtkim@gmail.com>
2016-11-10 11:21:56 -0800

    adds tour stop menu global component

---
+++ www/views/global-components/tour-stops-menu/tour-stops-menu.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+    angular.module('Template')
+        .component('tourStopsMenu', {
+            templateUrl: 'views/global-components/tour-stops-menu/tour-stops-menu.html',
+            controller: TourStopsMenuCtrl,
+            controllerAs: 'cc'
+        })
+
+    TourStopsMenuCtrl.$inject = ['$log', '$scope'];
+
+    function TourStopsMenuCtrl($log, $scope) {
+        // console.log('TourStops')
+        var cc = this;
+        cc.noNav = true;
+        cc.toggleNav = function () {
+            cc.noNav = !cc.noNav;
+            $scope.$emit('darkenNav');
+        };
+
+        cc.$onInit = function () {
+        }
+    }
+})();
