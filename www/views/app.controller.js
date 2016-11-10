f1f49f4a6a15cbbef2d0db1053c4da08acd84a12
Teembokeem <myungtkim@gmail.com>
2016-11-09 16:55:53 -0800

    adds scroll delegating for nonav onto the app level

--- www/views/app.controller.js
+++ www/views/app.controller.js
@@ -1,23 +1,34 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('Template')
         .controller('AppCtrl', AppCtrl);
 
-    function AppCtrl($scope, $state, $rootScope, $log) {
+    function AppCtrl($scope, $state, $rootScope, $log, $ionicScrollDelegate) {
 
         var vm = this;
 
         vm.contentTitle = $state.current.contentTitle;
         vm.poop = 'poop bindings'
 
-        $rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams, $state) {
+        $rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState, fromParams, $state) {
             // $rootScope.contentTitle = ;
             vm.contentTitle = toState.contentTitle;
 
-            $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+            $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
                 event.preventDefault();
             });
         });
+
+        var t = $ionicScrollDelegate.$getByHandle('mainScroll');
+        vm.getScroll = function () {
+            var fromTop = t.getScrollPosition().top;
+            if (fromTop > 50) {
+                $rootScope.$emit('noNav', true);
+            } else {
+                $rootScope.$emit('noNav', false);
+            }
+
+        }
     }
 })();
