85013ca9ad64d9339b933e55ff960a141a36fd63
Teembokeem <myungtkim@gmail.com>
2016-11-10 10:22:51 -0800

    resizing for partner images free

--- www/views/home/home-partner/Component.home-partner.js
+++ www/views/home/home-partner/Component.home-partner.js
@@ -19,39 +19,39 @@
 
             cc.partnerContent = [
                 {
-                    img: 'img/go90_LOGO.jpg',
-                    dim: 120,
+                    img: 'img/go90_LOGO.png',
+                    // dim: 120,
                     link: 'http://www.go90.com'
                 },
                 // 'img/partner-2.png',
                 {
                     img: 'img/coors-light-mountain-logo.png',
-                    dim: 100,
+                    // dim: 100,
                     link: 'http://www.coors.com'
                 },
                 {
                     img: 'img/Stoli.png',
-                    dim: 120,
+                    // dim: 120,
                     link: 'http://http://stoli.com/'
                 },
                 {
-                    img: 'img/CLIF.JPG',
-                    dim: 100,
+                    img: 'img/CLIF.png',
+                    // dim: 100,
                     link: 'http://www.clifbar.com'
                 },
                 {
                     img: 'img/foro_logo-2.png',
-                    dim: 120,
+                    // dim: 120,
                     link: 'http://www.theforo.com'
                 },
                 {
                     img: 'img/FLOOD_MAGAZINE_PRESENTS.png',
-                    dim: 120,
+                    // dim: 120,
                     link: 'http://floodmagazine.com'
                 },
                 {
                     img: 'img/Halfpops-Logo---Horizontal-Reversed.png',
-                    dim: 120,
+                    // dim: 120,
                     link: 'https://www.halfpops.com/'
                 },
             ];
