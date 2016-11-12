c451f718ff74be60ce94da694116562970cc7e17
Teembokeem <myungtkim@gmail.com>
2016-11-11 16:25:53 -0800

    adds tour music component

---
+++ www/views/tour-stops/tour-music/Component.tour-music.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+    angular.module('TourStops.view.module')
+        .component('tourMusic', {
+            bindings: {
+
+            },
+            templateUrl: 'views/tour-stops/tour-music/tour-music.html',
+            controller: tourMusicCtrl,
+            controllerAs: 'cc'
+        })
+
+    tourMusicCtrl.$inject = ['$log', '$rootScope', '$window', '$scope'];
+
+    function tourMusicCtrl($log, $rootScope, $window, $scope) {
+        var cc = this;
+
+        // cc.go = function (link) {
+        //     window.location = link;
+        // };
+
+        // $scope.$watch(function () {
+        //     return $window.innerWidth;
+        // }, function (a, b, c) {
+        //     // console.log(a, b, c)
+        //     if (a < 1024) {
+        //         cc.mobile = true;
+        //     } else {
+        //         cc.mobile = false;
+        //         // if (!$scope.$$phase) {
+        //         //     $scope.$apply(function () {
+        //         // if (!cc.mobile) {
+        //         // cc.noNav = ;
+        //         // }
+        //         //     });
+        //         // }
+        //     }
+
+        // })
+
+        cc.$onInit = function () {
+
+            cc.tourMusicContent = [
+                {
+                    name: 'BIFFY CLYRO',
+                    img: '../img/biffy.png'
+                },
+                {
+                    name: 'BEGINNER',
+                    img: '../img/beginner.png'
+                }
+            ]
+        }
+    }
+})();
