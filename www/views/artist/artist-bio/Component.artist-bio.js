d846647cf92f92420b856bbceb462b985c0596ef
Teembokeem <myungtkim@gmail.com>
2016-11-16 21:16:15 -0800

    adds bio content

--- www/views/artist/artist-bio/Component.artist-bio.js
+++ www/views/artist/artist-bio/Component.artist-bio.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
     angular.module('Artist.view.module')
         .component('artistBio', {
@@ -14,25 +14,18 @@
 
     function artistBioCtrl($log, $stateParams) {
         var cc = this;
+        cc.go = function(link) {
+            window.location = link;
+        };
 
-        cc.$onInit = function () {
+        cc.$onInit = function() {
 
-            cc.artistList = {
-                beginner: {
-                    name: 'BEGINNER',
-                    ref: 'beginner',
-                    location: 'main stage',
-                    time: 'fri 3 9pm',
-                    hero: '../img/beginner-hero.jpg'
-                },
-                clyro: {
-                    name: 'BIFFY CLYRO',
-                    ref: 'clyro',
-                    location: 'main stage',
-                    time: 'fri 3 6pm',
-                    hero: '../img/clyro-hero.jpg'
-
-                }
+            cc.artistBioContent = {
+                info: [
+                    'American electronic music band composed of record producer Diplo, Jillionaire, and Walshy fire. It was founded by Diplo and Switch, but Switch left in 2011. Its music spans numerous genres, mixing reggae with dancehall, raggaeton, house, and moombhaton.',
+                    'Major Lazer has released three full-length albums: Guns Don\'t Kill People... Lazers Do 2009, Free the Universe 2013,  and Peace Is the Mission in 2015. It also released an EP entitled Apocalypse Soon in March 2014, which featured vocal contribution from Pharrell Williams and Sean Paul. In addition, Major Lazer has also produced Reincarnated, the debut reggae album by Snoop Dogg under his \'Snoop Lion \' persona'
+                ],
+                website: 'http://www.beginner.de/'
             }
         }
     }
