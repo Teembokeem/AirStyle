01455e70641a71f3833916c1ab21dd6f745c095b
Teembokeem <myungtkim@gmail.com>
2016-11-16 21:15:47 -0800

    adds handling for stateparams for artist view module

--- www/views/global-components/video-comp/Component.video-comp.js
+++ www/views/global-components/video-comp/Component.video-comp.js
@@ -1,27 +1,28 @@
-(function () {
+(function() {
     'use strict';
     angular.module('Template')
         .component('videoComp', {
             bindings: {
-
             },
             templateUrl: 'views/global-components/video-comp/video-comp.html',
             controller: videoCompCtrl,
             controllerAs: 'cc'
         })
 
-    videoCompCtrl.$inject = ['$log', '$rootScope', '$window', '$scope'];
+    videoCompCtrl.$inject = ['$log', '$rootScope', '$window', '$scope', '$stateParams', '$sce'];
 
-    function videoCompCtrl($log, $rootScope, $window, $scope) {
+    function videoCompCtrl($log, $rootScope, $window, $scope, $stateParams, $sce) {
         var cc = this;
+
+        cc.link = 0;
 
         // cc.go = function (link) {
         //     window.location = link;
         // };
 
-        $scope.$watch(function () {
+        $scope.$watch(function() {
             return $window.innerWidth;
-        }, function (a, b, c) {
+        }, function(a, b, c) {
             cc.width = 0.90 * $window.innerWidth;
             cc.length = 315 / 560 * cc.width;
             //     // console.log(a, b, c)
@@ -40,7 +41,17 @@
 
         })
 
-        cc.$onInit = function () {
+        var videos = {
+            beginner: 'https://www.youtube.com/embed/isMx-EISg3E',
+            clyro: 'https://www.youtube.com/embed/bpJvP4b5fX8'
+        }
+
+        cc.$onInit = function() {
+            if ($stateParams.artist) {
+                cc.link = $sce.trustAsResourceUrl(videos[$stateParams.artist])
+                console.log($stateParams.artist, videos[$stateParams.artist], videos, cc.link)
+            }
+
         }
     }
 })();
