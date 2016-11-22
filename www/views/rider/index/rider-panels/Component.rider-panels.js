3eefda4a644b08b9b42bb5ef8cfaf72267a56cf2
Teembokeem <myungtkim@gmail.com>
2016-11-21 16:58:11 -0800

    boiler template for rider panels component

--- www/views/rider/index/rider-panels/Component.rider-panels.js
+++ www/views/rider/index/rider-panels/Component.rider-panels.js
@@ -10,37 +10,48 @@
             controllerAs: 'cc'
         })
 
-    riderPanelsCtrl.$inject = ['$log', '$rootScope', '$window', '$scope'];
+    riderPanelsCtrl.$inject = ['$log', '$state'];
 
-    function riderPanelsCtrl($log, $rootScope, $window, $scope) {
+    function riderPanelsCtrl($log, $state) {
         var cc = this;
 
+        cc.stateChange = stateChange;
+
+        function stateChange(state, param, val) {
+            $state.go(state, { param: val });
+        }
 
         cc.$onInit = function () {
 
             cc.riderPanelsContent = [
                 {
                     name: 'Sven Thorgren',
+                    ref: 'sthorgren',
                     image: '../img/Sven-Thorgren.jpg'
                 },
                 {
                     name: 'Antoine Truchon',
+                    ref: 'atruchon',
                     image: '../img/Antoine-Truchon.jpg'
                 },
                 {
                     name: 'Max Parrot',
+                    ref: 'mparrot',
                     image: '../img/Max-Parrot.jpg'
                 },
                 {
                     name: 'Keita Inamura',
+                    ref: 'kinamura',
                     image: '../img/Keita-Inamura.jpg'
                 },
                 {
                     name: 'Billy Morgan',
+                    ref: 'bmorgan',
                     image: '../img/Billy-Morgan.jpg'
                 },
                 {
                     name: 'Torgeir Bergrem',
+                    ref: 'tbergrem',
                     image: '../img/Torgeir-Bergrem.jpg'
                 }
             ]
