7594ad24a3e83af2ff5b35bfbf214b3696a48b4b
Teembokeem <myungtkim@gmail.com>
2016-11-23 11:31:59 -0800

    adds logic for handling filters by event and year

--- www/views/rankings/rankings-list/Component.rankings-list.js
+++ www/views/rankings/rankings-list/Component.rankings-list.js
@@ -14,26 +14,29 @@
 
     function rankingsListCtrl($log) {
         var cc = this;
+
+        cc.currentFilter = 'overall';
+        cc.currentYear = '2016';
         cc.filterAthletes = filterAthletes;
 
         function filterAthletes(param) {
             if (param == 'overall') {
                 cc.filterList = cc.filterList.sort(function (a, b) {
-                    return b.overall - a.overall
-                })
+                    return b.scores[cc.currentYear].overall - a.scores[cc.currentYear].overall
+                });
             }
             else {
-                var indexObj = cc.filterList[0].scores.filter(function (score) {
-                    return score.event == param
-                })[0];
-
-                var index = cc.filterList[0].scores.indexOf(indexObj);
 
                 cc.filterList = cc.filterList.sort(function (a, b) {
-                    return b.scores[index].score - a.scores[index].score
+                    return b.scores[cc.currentYear][param].score - a.scores[cc.currentYear][param].score
                 })
             }
+            cc.currentFilter = param;
         };
+
+        function chooseOption() {
+            console.log('sup')
+        }
 
         cc.$onInit = function () {
 
@@ -43,125 +46,227 @@
                     ref: 'sthorgren',
                     image: '../img/Sven-Thorgren.jpg',
                     origin: 'Sweden',
-                    scores: [
-                        {
-                            event: 'beijing',
-                            score: 84.61
+                    scores: {
+                        '2015': {
+                            overall: '201.76',
+                            beijing: {
+                                score: 79.61,
+                                eventName: 'Beijing'
+                            },
+                            innsbruck: {
+                                score: 62.54,
+                                eventName: 'Innsbruck'
+                            },
+                            la: {
+                                score: 59.61,
+                                eventName: 'Los Angeles'
+                            }
                         },
-                        {
-                            event: 'innsbruck',
-                            score: 66.54
-                        },
-                        {
-                            event: 'los angeles',
-                            score: 84.61
+                        '2016': {
+                            overall: '235.75',
+                            beijing: {
+                                eventName: 'beijing',
+                                score: 84.61
+                            },
+                            innsbruck: {
+                                eventName: 'innsbruck',
+                                score: 66.54
+                            },
+                            la: {
+                                eventName: 'los angeles',
+                                score: 84.61
+                            }
                         }
-                    ],
-                    overall: '235.75'
+                    },
                 },
                 {
                     name: 'Antoine Truchon',
                     ref: 'atruchon',
                     image: '../img/Antoine-Truchon.jpg',
                     origin: 'Sweden',
-                    scores: [
-                        {
-                            event: 'beijing',
-                            score: 52.35
+                    scores: {
+                        '2015': {
+                            overall: '205.54',
+                            beijing: {
+                                score: 52.35,
+                                eventName: 'Beijing'
+                            },
+                            innsbruck: {
+                                score: 48.19,
+                                eventName: 'Innsbruck'
+                            },
+                            la: {
+                                score: 100.00,
+                                eventName: 'Los Angeles'
+                            }
                         },
-                        {
-                            event: 'innsbruck',
-                            score: 48.19
-                        },
-                        {
-                            event: 'los angeles',
-                            score: 100.00
+                        '2016': {
+                            overall: '205.72',
+                            beijing: {
+                                eventName: 'beijing',
+                                score: 42.61
+                            },
+                            innsbruck: {
+                                eventName: 'innsbruck',
+                                score: 84.79
+                            },
+                            la: {
+                                eventName: 'los angeles',
+                                score: 78.32
+                            }
                         }
-                    ],
-                    overall: '200.54'
+                    },
                 },
                 {
                     name: 'Max Parrot',
                     ref: 'mparrot',
                     image: '../img/Max-Parrot.jpg',
                     origin: 'Sweden',
-                    scores: [
-                        {
-                            event: 'beijing',
-                            score: 72.03
+                    scores: {
+                        '2015': {
+                            overall: '238.16',
+                            beijing: {
+                                score: 63.47,
+                                eventName: 'Beijing'
+                            },
+                            innsbruck: {
+                                score: 92.49,
+                                eventName: 'Innsbruck'
+                            },
+                            la: {
+                                score: 82.20,
+                                eventName: 'Los Angeles'
+                            }
                         },
-                        {
-                            event: 'innsbruck',
-                            score: 78.02
-                        },
-                        {
-                            event: 'los angeles',
-                            score: 42.26
+                        '2016': {
+                            overall: '194.31',
+                            beijing: {
+                                eventName: 'beijing',
+                                score: 72.03
+                            },
+                            innsbruck: {
+                                eventName: 'innsbruck',
+                                score: 78.02
+                            },
+                            la: {
+                                eventName: 'los angeles',
+                                score: 42.26
+                            }
                         }
-                    ],
-                    overall: '194.31'
+                    },
                 },
                 {
                     name: 'Keita Inamura',
                     ref: 'kinamura',
                     image: '../img/Keita-Inamura.jpg',
                     origin: 'Sweden',
-                    scores: [
-                        {
-                            event: 'beijing',
-                            score: 66.54
+                    scores: {
+                        '2015': {
+                            overall: '221.40',
+                            beijing: {
+                                score: 72.01,
+                                eventName: 'Beijing'
+                            },
+                            innsbruck: {
+                                score: 69.20,
+                                eventName: 'Innsbruck'
+                            },
+                            la: {
+                                score: 80.19,
+                                eventName: 'Los Angeles'
+                            }
                         },
-                        {
-                            event: 'innsbruck',
-                            score: 52.35
-                        },
-                        {
-                            event: 'los angeles',
-                            score: 66.54
+                        '2016': {
+                            overall: '185.43',
+                            beijing: {
+                                eventName: 'beijing',
+                                score: 66.54
+                            },
+                            innsbruck: {
+                                eventName: 'innsbruck',
+                                score: 52.35
+                            },
+                            la: {
+                                eventName: 'los angeles',
+                                score: 66.54
+                            }
                         }
-                    ],
-                    overall: '185.43'
+                    },
                 },
                 {
                     name: 'Billy Morgan',
                     ref: 'bmorgan',
                     image: '../img/Billy-Morgan.jpg',
-                    scores: [
-                        {
-                            event: 'beijing',
-                            score: 78.02
+                    scores: {
+                        '2015': {
+                            overall: '223.29',
+                            beijing: {
+                                score: 60.28,
+                                eventName: 'Beijing'
+                            },
+                            innsbruck: {
+                                score: 95.38,
+                                eventName: 'Innsbruck'
+                            },
+                            la: {
+                                score: 67.63,
+                                eventName: 'Los Angeles'
+                            }
                         },
-                        {
-                            event: 'innsbruck',
-                            score: 5.72
-                        },
-                        {
-                            event: 'los angeles',
-                            score: 100.00
+                        '2016': {
+                            overall: '218.52',
+                            beijing: {
+                                eventName: 'beijing',
+                                score: 78.02
+                            },
+                            innsbruck: {
+                                eventName: 'innsbruck',
+                                score: 58.16
+                            },
+                            la: {
+                                eventName: 'los angeles',
+                                score: 82.34
+                            }
                         }
-                    ],
-                    overall: '183.74'
+                    },
                 },
                 {
                     name: 'Torgeir Bergrem',
                     ref: 'tbergrem',
                     image: '../img/Torgeir-Bergrem.jpg',
                     origin: 'Sweden',
-                    scores: [
-                        {
-                            event: 'beijing',
-                            score: 91.89
+                    scores: {
+                        '2015': {
+                            overall: '215.79',
+                            beijing: {
+                                score: 49.77,
+                                eventName: 'Beijing'
+                            },
+                            innsbruck: {
+                                score: 83.29,
+                                eventName: 'Innsbruck'
+                            },
+                            la: {
+                                score: 82.73,
+                                eventName: 'Los Angeles'
+                            }
                         },
-                        {
-                            event: 'innsbruck',
-                            score: 44.26
-                        },
-                        {
-                            event: 'los angeles',
-                            score: 33.45
+                        '2016': {
+                            overall: '169.90',
+                            beijing: {
+                                eventName: 'beijing',
+                                score: 91.89
+                            },
+                            innsbruck: {
+                                eventName: 'innsbruck',
+                                score: 44.26
+                            },
+                            la: {
+                                eventName: 'los angeles',
+                                score: 33.45
+                            }
                         }
-                    ],
-                    overall: '169.90'
+                    },
                 }
             ]
 
