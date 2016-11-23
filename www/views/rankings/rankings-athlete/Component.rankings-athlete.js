2875a507c764b0cb8928ecf13499983bccd0a2b1
Teembokeem <myungtkim@gmail.com>
2016-11-23 11:32:09 -0800

    update data bindings

--- www/views/rankings/rankings-athlete/Component.rankings-athlete.js
+++ www/views/rankings/rankings-athlete/Component.rankings-athlete.js
@@ -1,9 +1,10 @@
-(function () {
+(function() {
     'use strict';
     angular.module('Rankings.view.module')
         .component('rankingsAthlete', {
             bindings: {
                 athlete: '=',
+                year: '=',
                 index: '='
             },
             templateUrl: 'views/rankings/rankings-athlete/rankings-athlete.html',
@@ -16,25 +17,8 @@
     function rankingsAthleteCtrl($log) {
         var cc = this;
 
-        cc.$onInit = function () {
+        cc.$onInit = function() {
 
-            cc.scheduleContent = [
-                {
-                    title: 'BEIJING',
-                    date: 'NOV 18 + 19',
-                    dest: 'app.here'
-                },
-                {
-                    title: 'INNSBRUCK',
-                    date: 'FEB 3 + 4',
-                    dest: 'app.there'
-                },
-                {
-                    title: 'LOS ANGELES',
-                    date: 'FEB 18 + 19',
-                    dest: 'app.anywhere'
-                },
-            ]
         }
     }
 })();
