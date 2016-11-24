e99605564ad4d89f0e70c3d26ad2288cc66d7aaa
Teembokeem <myungtkim@gmail.com>
2016-11-23 16:23:55 -0800

    adds boiler for rider bio segment

--- www/views/rider/details/rider-bio/Component.rider-bio.js
+++ www/views/rider/details/rider-bio/Component.rider-bio.js
@@ -10,9 +10,9 @@
             controllerAs: 'cc'
         })
 
-    riderBioCtrl.$inject = ['$log', '$state'];
+    riderBioCtrl.$inject = ['$log', '$state', '$stateParams'];
 
-    function riderBioCtrl($log, $state) {
+    function riderBioCtrl($log, $state, $stateParams) {
         var cc = this;
 
         cc.stateChange = stateChange;
@@ -25,19 +25,101 @@
 
             cc.riderBioContent = [
                 {
-                    name: 'Yuki Kadono',
-                    ref: 'ykadono',
+                    name: 'Torgeir Bergrem',
+                    ref: 'tbergrem',
+                    image: '../img/Torgeir-Bergrem.jpg',
                     dob: 832402800000,
-                    Nation: 'Japan',
-                    Stance: Regular,
+                    nation: 'Japan',
+                    stance: 'Regular',
                     fb: '',
                     tweet: '',
                     inst: '',
                     youtube: '',
-                    asYears: ['2017', '2016', '2015', '2013', '2013'],
-                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.']
-                }
+                    asYears: ['2017', '2016', '2015', '2013'],
+                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.'],
+                    sponsors: ['sponsor a', 'sponsor b', 'sponsor c']
+                },
+                {
+                    name: 'Sven Thorgren',
+                    ref: 'sthorgren',
+                    image: '../img/Sven-Thorgren.jpg',
+                    dob: 832402800000,
+                    nation: 'Japan',
+                    stance: 'Regular',
+                    fb: '',
+                    tweet: '',
+                    inst: '',
+                    youtube: '',
+                    asYears: ['2017', '2016', '2015', '2013'],
+                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.'],
+                    sponsors: ['sponsor a', 'sponsor b', 'sponsor c']
+                },
+                {
+                    name: 'Antoine Truchon',
+                    ref: 'atruchon',
+                    image: '../img/Antoine-Truchon.jpg',
+                    dob: 832402800000,
+                    nation: 'Japan',
+                    stance: 'Regular',
+                    fb: '',
+                    tweet: '',
+                    inst: '',
+                    youtube: '',
+                    asYears: ['2017', '2016', '2015', '2013'],
+                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.'],
+                    sponsors: ['sponsor a', 'sponsor b', 'sponsor c']
+                },
+                {
+                    name: 'Max Parrot',
+                    ref: 'mparrot',
+                    image: '../img/Max-Parrot.jpg',
+                    dob: 832402800000,
+                    nation: 'Japan',
+                    stance: 'Regular',
+                    fb: '',
+                    tweet: '',
+                    inst: '',
+                    youtube: '',
+                    asYears: ['2017', '2016', '2015', '2013'],
+                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.'],
+                    sponsors: ['sponsor a', 'sponsor b', 'sponsor c']
+                },
+                {
+                    name: 'Keita Inamura',
+                    ref: 'kinamura',
+                    image: '../img/Keita-Inamura.jpg',
+                    dob: 832402800000,
+                    nation: 'Japan',
+                    stance: 'Regular',
+                    fb: '',
+                    tweet: '',
+                    inst: '',
+                    youtube: '',
+                    asYears: ['2017', '2016', '2015', '2013'],
+                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.'],
+                    sponsors: ['sponsor a', 'sponsor b', 'sponsor c']
+                },
+                {
+                    name: 'Billy Morgan',
+                    ref: 'bmorgan',
+                    image: '../img/Billy-Morgan.jpg',
+                    dob: 832402800000,
+                    nation: 'Japan',
+                    stance: 'Regular',
+                    fb: '',
+                    tweet: '',
+                    inst: '',
+                    youtube: '',
+                    asYears: ['2017', '2016', '2015', '2013'],
+                    bio: ['Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapubus ac facilisis in, egestas eget quam. Praaesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo dio, dapibus ac ficisilisis in,  egestas eget quam.', 'Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Donec sed odiod dui. Donec ullamcoper nulla non metus auctor fringilla.'],
+                    sponsors: ['sponsor a', 'sponsor b', 'sponsor c']
+                },
             ]
+
+            cc.rider = cc.riderBioContent.filter(function (rider) {
+                return rider.ref == $stateParams.rider
+            })[0];
+            console.log(cc.rider)
         }
     }
 })();
