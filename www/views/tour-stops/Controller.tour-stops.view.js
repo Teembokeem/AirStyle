990a98249a3eb9b688b7311861b0a98f8b195dfd
Teembokeem <myungtkim@gmail.com>
2016-12-07 14:30:12 -0800

    adds dynamic data

--- www/views/tour-stops/Controller.tour-stops.view.js
+++ www/views/tour-stops/Controller.tour-stops.view.js
@@ -5,9 +5,17 @@
         .module('TourStops.view.module')
         .controller('TourStopsCtrl', TourStopsCtrl);
 
-    TourStopsCtrl.$inject = ['$log', '$state', '$rootScope'];
+    TourStopsCtrl.$inject = ['$log', '$state', '$rootScope', 'Artists', '$stateParams'];
 
-    function TourStopsCtrl($log, $state, $rootScope) {
+    function TourStopsCtrl($log, $state, $rootScope, Artists, $stateParams) {
         var cc = this;
+
+        cc.avatars = Artists($stateParams.tour);
+        cc.showSchedule = true;
+        cc.showViewAll = true;
+
+            console.log($stateParams.tour);
+            console.log(Artists(), 'hello');
+
     }
 })();
