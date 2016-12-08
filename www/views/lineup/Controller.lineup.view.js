c846a52211fd3b69f47469b6d5fa6d5ecc3404bf
Teembokeem <myungtkim@gmail.com>
2016-12-07 16:34:04 -0800

    adds back to button with data bindings

--- www/views/lineup/Controller.lineup.view.js
+++ www/views/lineup/Controller.lineup.view.js
@@ -5,10 +5,24 @@
         .module('Lineup.view.module')
         .controller('LineupCtrl', LineupCtrl);
 
-    LineupCtrl.$inject = ['$log', '$state', '$rootScope'];
+    LineupCtrl.$inject = ['$log', '$state', '$rootScope', '$stateParams'];
 
-    function LineupCtrl($log, $state, $rootScope) {
+    function LineupCtrl($log, $state, $rootScope, $stateParams) {
         var cc = this;
+
+        cc.codex = {
+            la: 'Los Angeles',
+            beijing: 'Beijing',
+            innsbruck: 'Innsbruck'
+        }
+        cc.backState = $stateParams.tour;
+        cc.backRef = cc.codex[cc.backState];
+
+        cc.stateChange = stateChange;
+
+        function stateChange(state, val) {
+            val ? $state.go(state, { tour: val }) : $state.go(state);
+        };
 
         cc.avatars = [
             {
