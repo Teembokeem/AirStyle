54765341ae5ce4abf94bc3010b5e9fff0eb536c3
Teembokeem <myungtkim@gmail.com>
2016-12-08 16:20:19 -0800

    adds schedule main view module

---
+++ www/views/schedule/Module.schedule.view.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+    var dir = "views/lineup/"
+    angular.module('Schedule.view.module', ['Services'])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('app.schedule', {
+                    url: '/schedule/:tour',
+                    views: {
+                        'content': {
+                            templateUrl: dir + 'schedule.view.html',
+                            controller: 'ScheduleCtrl',
+                            controllerAs: 'cc'
+                        }
+                    },
+                    params: {
+                        tour: { value: 'innsbruck' }
+                    }
+                })
+
+        })
+
+
+        .run(function ($log) {
+        });
+})();
