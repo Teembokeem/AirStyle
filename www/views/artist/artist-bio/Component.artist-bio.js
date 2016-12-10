2cf9cf85f9a0cc135d76917a7a4f9a9c296328e4
Teembokeem <myungtkim@gmail.com>
2016-12-09 23:58:48 -0800

    data binding fixes, adds static data service for artist bio

--- www/views/artist/artist-bio/Component.artist-bio.js
+++ www/views/artist/artist-bio/Component.artist-bio.js
@@ -10,23 +10,17 @@
             controllerAs: 'cc'
         })
 
-    artistBioCtrl.$inject = ['$log', '$stateParams'];
+    artistBioCtrl.$inject = ['$log', '$stateParams', 'Artists', '$sce'];
 
-    function artistBioCtrl($log, $stateParams) {
+    function artistBioCtrl($log, $stateParams, Artists, $sce) {
         var cc = this;
         cc.go = function(link) {
             window.location = link;
         };
 
         cc.$onInit = function() {
-
-            cc.artistBioContent = {
-                info: [
-                    'American electronic music band composed of record producer Diplo, Jillionaire, and Walshy fire. It was founded by Diplo and Switch, but Switch left in 2011. Its music spans numerous genres, mixing reggae with dancehall, raggaeton, house, and moombhaton.',
-                    'Major Lazer has released three full-length albums: Guns Don\'t Kill People... Lazers Do 2009, Free the Universe 2013,  and Peace Is the Mission in 2015. It also released an EP entitled Apocalypse Soon in March 2014, which featured vocal contribution from Pharrell Williams and Sean Paul. In addition, Major Lazer has also produced Reincarnated, the debut reggae album by Snoop Dogg under his \'Snoop Lion \' persona'
-                ],
-                website: 'http://www.beginner.de/'
-            }
+            cc.artist = Artists($stateParams.avatar);
+            cc.artist.spotify = $sce.trustAsResourceUrl(cc.artist.spotify);
         }
     }
 })();
