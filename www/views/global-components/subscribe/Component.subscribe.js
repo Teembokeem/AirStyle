d6acfe5fa5eb7bf202c02ad0b46c8752e60023cc
Teembokeem <myungtkim@gmail.com>
2016-12-20 17:03:45 -0800

    adds better logic handling for subscriber component. todo: add in the mailchimp call  to the sbuscribe container

--- www/views/global-components/subscribe/Component.subscribe.js
+++ www/views/global-components/subscribe/Component.subscribe.js
@@ -10,21 +10,35 @@
             controllerAs: 'cc'
         })
 
-    SubscribeCtrl.$inject = ['$log'];
+    SubscribeCtrl.$inject = ['$log', '$window'];
 
-    function SubscribeCtrl($log) {
+    function SubscribeCtrl($log, $window) {
         var cc = this;
 
         cc.focused = focused;
         cc.newUserEmail = '';
+        cc.grabText = grabText;
+        cc.clearText = clearText;
 
         function focused() {
             cc.newUserEmail = '';
         }
+
+        function grabText() {
+            console.log("supo dude")
+            $window.innerWidth < 550 ? cc.placeholder = "Don't miss out! Subscribe Today." : cc.placeholder = "Don't miss a thing! Stay up to date by subscribing to our newsletter";
+            return cc.placeholder;
+        };
+
+        function clearText() {
+            cc.placeholder = '';
+            return cc.placeholder;
+        }
+
         cc.$onInit = function () {
 
-            cc.subscribeContent = [
-            ]
+            grabText();
+
         }
     }
 })();
