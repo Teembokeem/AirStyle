3aa8b8a14314074f64ac4466db3bd3e8b9c7f637
Teembokeem <myungtkim@gmail.com>
2016-12-31 04:12:43 -0800

    adds heading for video comps on artist bio pages

--- www/views/artist/Controller.artist.view.js
+++ www/views/artist/Controller.artist.view.js
@@ -5,9 +5,14 @@
         .module('Artist.view.module')
         .controller('ArtistCtrl', ArtistCtrl);
 
-    ArtistCtrl.$inject = ['$log', '$state', '$rootScope'];
+    ArtistCtrl.$inject = ['$log', '$state', '$rootScope', 'Artists', '$stateParams'];
 
-    function ArtistCtrl($log, $state, $rootScope) {
+    function ArtistCtrl($log, $state, $rootScope, Artists, $stateParams) {
         var cc = this;
+
+
+        console.log('avatar', $stateParams)
+        cc.video = Artists($stateParams.avatar).video;
+
     }
 })();
