2df673c128d4c1a9c6cf4f70b97d730f605517c1
Teembokeem <myungtkim@gmail.com>
2016-12-31 07:14:53 -0800

    adds handling for opening window content for merhcandise

--- www/views/home/home-merchandise/Component.home-merchandise.js
+++ www/views/home/home-merchandise/Component.home-merchandise.js
@@ -16,13 +16,18 @@
         var cc = this;
 
         cc.openMerch = function (merch) {
-            window.open("http://www.air-style.clothing/collections/2017-merch/products/" + merch);
+            if (merch) {
+                return window.open("http://www.air-style.clothing/collections/2017-merch/products" + merch);
+            } else {
+                window.open("http://www.air-style.clothing/collections/2017-merch");
+            }
         };
         cc.borderButtonHover = false;
 
         cc.$onInit = function () {
 
             cc.merchandiseContent = Merchandise();
+            console.log(cc.merchandiseContent)
         }
     }
 })();
