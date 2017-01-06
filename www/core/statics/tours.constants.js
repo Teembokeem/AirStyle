91e6a64e5d9d324005dd148b168b95a11ba108fe
Teembokeem <myungtkim@gmail.com>
2017-01-05 21:04:04 -0800

    adds schedule to tour constant

--- www/core/statics/tours.constants.js
+++ www/core/statics/tours.constants.js
@@ -17,7 +17,164 @@
                     ref: 'innsbruck',
                     date_semantic: 'FEB 3 + 4',
                     dest: 'app.tour-stops',
-                    video: 'https://www.youtube.com/embed/PhBNiSlcbRk'
+                    video: 'https://www.youtube.com/embed/PhBNiSlcbRk',
+                    schedule: [
+                        {
+                            date: 'Fri Feb 3',
+                            stages: [
+                                {
+                                    name: 'Ramp',
+                                    lineup: [
+                                        {
+                                            name: 'Qualification Men Run 1',
+                                            ref: '',
+                                            time_mult: 20,
+                                            time_dur: 4,
+                                            time_semantic: '18:00'
+                                        },
+                                        {
+                                            name: 'Qualification Men Run 2',
+                                            ref: '',
+                                            time_mult: 28,
+                                            time_dur: 4,
+                                            time_semantic: '20:00'
+                                        },
+                                    ]
+                                },
+                                {
+                                    name: 'Main Stage',
+                                    lineup: [
+                                        {
+                                            name: 'The Hunna',
+                                            ref: 'the-hunna',
+                                            time_mult: 16,
+                                            time_dur: 2,
+                                            time_semantic: '17:00'
+                                        },
+                                        {
+                                            name: 'Millencolin',
+                                            ref: 'millencolin',
+                                            time_mult: 23,
+                                            time_dur: 3,
+                                            time_semantic: '18:45'
+                                        },
+                                        {
+                                            name: 'Bilderbuch',
+                                            ref: 'bilderbuch',
+                                            time_mult: 31,
+                                            time_dur: 5,
+                                            time_semantic: '20:45'
+                                        }
+                                    ]
+                                },
+                            ]
+                        },
+                        {
+                            date: 'Sat Feb 4',
+                            stages: [
+                                {
+                                    name: 'Ramp',
+                                    lineup: [
+                                        {
+                                            name: 'Semifinals Men Run 1 - 3',
+                                            ref: '',
+                                            time_mult: 2,
+                                            time_dur: 6,
+                                            time_semantic: '13:30'
+                                        },
+                                        {
+                                            name: 'Qualification Women Run 1-3',
+                                            ref: '',
+                                            time_mult: 12,
+                                            time_dur: 4,
+                                            time_semantic: '16:00'
+                                        },
+                                        {
+                                            name: 'Finals Women',
+                                            ref: '',
+                                            time_mult: 26,
+                                            time_dur: 2,
+                                            time_semantic: '19:30'
+                                        },
+                                        {
+                                            name: 'Finals Men',
+                                            ref: '',
+                                            time_mult: 28,
+                                            time_dur: 4,
+                                            time_semantic: '20:00'
+                                        },
+                                    ]
+                                },
+                                {
+                                    name: 'Main Stage',
+                                    lineup: [
+                                        {
+                                            name: 'SSIO',
+                                            ref: 'ssio',
+                                            time_mult: 8,
+                                            time_dur: 4,
+                                            time_semantic: '15:00'
+                                        },
+                                        {
+                                            name: 'The Naked And Famous',
+                                            ref: 'the-naked-and-famous',
+                                            time_mult: 17,
+                                            time_dur: 3,
+                                            time_semantic: '17:15'
+                                        },
+                                        {
+                                            name: 'Biffy Clyro',
+                                            ref: 'clyro',
+                                            time_mult: 21,
+                                            time_dur: 3,
+                                            time_semantic: '18:30'
+                                        },
+                                        {
+                                            name: 'Beginner',
+                                            ref: 'beginner',
+                                            time_mult: 32,
+                                            time_dur: 4,
+                                            time_semantic: '21:00'
+                                        }
+                                    ]
+                                },
+                                {
+                                    name: 'Indoor Stage',
+                                    lineup: [
+                                        {
+                                            name: 'Counterfeit',
+                                            ref: 'counterfeit',
+                                            time_mult: 1,
+                                            time_dur: 3,
+                                            time_semantic: '13:15'
+                                        },
+                                        {
+                                            name: 'Lucky Chops',
+                                            ref: 'lucky-chops',
+                                            time_mult: 6,
+                                            time_dur: 4,
+                                            time_semantic: '14:30'
+                                        },
+                                        {
+                                            name: '257ers',
+                                            ref: '257ers',
+                                            time_mult: 13,
+                                            time_dur: 4,
+                                            time_semantic: '16:15'
+                                        },
+                                        {
+                                            name: 'F\xFCnf Sterne Deluxe',
+                                            ref: 'funf-sterne-deluxe',
+                                            time_mult: 19,
+                                            time_dur: 4,
+                                            time_semantic: '17:45'
+                                        }
+                                    ]
+                                },
+                            ]
+                        }
+
+                    ]
                 },
                 'la': {
                     title: 'LOS ANGELES',
