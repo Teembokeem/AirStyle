9ab4161dfe3c57e6f4c61699ca5ddf92a7a9d149
Teembokeem <myungtkim@gmail.com>
2017-01-06 05:08:42 -0800

    moves data to constant

--- www/views/faq/faq-general/Component.faq-general.js
+++ www/views/faq/faq-general/Component.faq-general.js
@@ -10,74 +10,13 @@
             controllerAs: 'cc'
         })
 
-    faqGeneralCtrl.$inject = ['$log'];
+    faqGeneralCtrl.$inject = ['$log', 'Tours', '$stateParams'];
 
-    function faqGeneralCtrl($log) {
+    function faqGeneralCtrl($log, Tours, $stateParams) {
         var cc = this;
 
         cc.$onInit = function () {
-            cc.faqGeneralContent = [
-                {
-                    question: 'Where is the Air+Style Los Angeles Festival located?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'What are the festival hours?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'What are the box office hours?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Can any age guest attend this festival?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Will there be lockers at the festival?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Is the festival handicap accessible?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Will there be food at the festival?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Do vendors accept debit/credit cards?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'What does the "cashless" option mean and how do I add funds?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Where can I get a program guide?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Where can I find a festival map?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Is there parking available at the festival?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'What is the nearest metro stop/bus stop to the festival entrance?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'Will the festival stay open if there is rain?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-                {
-                    question: 'I lost something inside, what do I do?',
-                    answer: '- - - - - - - - - - - - - -'
-                },
-            ]
+            cc.faqGeneralContent = Tours($stateParams.tour).faq_general;
         }
     }
 })();
