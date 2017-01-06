1f13698e2b8e58fcf7e1f76667e25fe48ed0dcba
Teembokeem <myungtkim@gmail.com>
2017-01-05 21:05:54 -0800

    adds header notification component for future use

---
+++ www/views/global-components/header-notification/Component.header-notification.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+    angular.module('Template')
+        .component('headerNotification', {
+            bindings: {
+
+            },
+            templateUrl: 'views/global-components/header-notification/header-notification.html',
+            controller: HeaderNotificationCtrl,
+            controllerAs: 'cc'
+        })
+
+    HeaderNotificationCtrl.$inject = [];
+
+    function HeaderNotificationCtrl() {
+        var cc = this;
+
+        cc.hovered = false;
+
+        cc.toastOpen = function () {
+            if (cc.hovered == false) {
+                cc.hovered = true;
+                swal({ title: '', text: 'More Content.', type: 'warning', timer: 2500 });
+                setTimeout(function () {
+                    cc.hovered = false;
+                }, 3000)
+            }
+        };
+
+        cc.$onInit = function () {
+            console.log("were in here")
+        }
+    }
+})();
