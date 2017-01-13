0b74442a3c13fa05570398f1560071f28337abf2
Teembokeem <myungtkim@gmail.com>
2017-01-13 15:55:16 -0800

    add afterparty and schedule mobile debugging

--- www/views/schedule/schedule-mobile/Component.schedule-mobile.js
+++ www/views/schedule/schedule-mobile/Component.schedule-mobile.js
@@ -10,15 +10,19 @@
             controllerAs: 'cc'
         })
 
-    scheduleMobileCtrl.$inject = ['$log', '$rootScope', '$window', '$scope'];
+    scheduleMobileCtrl.$inject = ['$log', '$rootScope', '$window', '$scope', 'Tours', '$stateParams'];
 
-    function scheduleMobileCtrl($log, $rootScope, $window, $scope) {
+    function scheduleMobileCtrl($log, $rootScope, $window, $scope, Tours, $stateParams) {
         var cc = this;
 
         cc.timeSlots = ['1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']
+        cc.currentChange;
 
         cc.go = function (link) {
             window.open(link);
+        };
+
+        cc.changeStage = function () {
         };
 
         $scope.$watch(function () {
@@ -36,249 +40,16 @@
                 // }
                 //     });
                 // }
-            }
+            };
 
-        })
+        });
 
         cc.$onInit = function () {
             // console.log('schedule mobile')
-
-            cc.ScheduleContent = [{
-                date: 'Fri Feb 3',
-                stages: [{
-                    name: 'Indoor Stage',
-                    lineup: [{
-                        name: 'Bilderbuch',
-                        ref: 'bilderbuch',
-                        time_mult: 2,
-                        time_dur: 6,
-                        time_semantic: '1:30pm'
-                    }, {
-                        name: 'Funf Sterne Deluxe',
-                        ref: 'funf-sterne-deluxe',
-                        time_mult: 13,
-                        time_dur: 7,
-                        time_semantic: '4:15pm'
-                    }, {
-                        name: 'The Naked and Famous',
-                        ref: 'the-naked-and-famous',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    }, {
-                        name: 'Biffy Clyro',
-                        ref: 'biffy-clyro',
-                        time_mult: 28,
-                        time_dur: 6,
-                        time_semantic: '8pm'
-                    }]
-                }, {
-                    name: 'Main Stage',
-                    lineup: [{
-                        name: 'Wax Wreckaz',
-                        ref: 'wax-wreckaz',
-                        time_mult: 4,
-                        time_dur: 4,
-                        time_semantic: '2:15pm'
-                    }, {
-                        name: 'Famouse Deck Team',
-                        ref: 'famouse-deck-team',
-                        time_mult: 11,
-                        time_dur: 7,
-                        time_semantic: '3:45pm'
-                    }, {
-                        name: 'Lucky Chops',
-                        ref: 'lucky-chops',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    }, {
-                        name: 'Beginner',
-                        ref: 'beginner',
-                        time_mult: 32,
-                        time_dur: 8,
-                        time_semantic: '9pm'
-                    }]
-                }, {
-                    name: 'Ramp',
-                    lineup: [{
-                        name: 'Qualification Round 1',
-                        ref: '',
-                        time_mult: 4,
-                        time_dur: 6,
-                        time_semantic: '2:15pm'
-                    }, {
-                        name: 'Qualification Round 2',
-                        ref: '',
-                        time_mult: 12,
-                        time_dur: 6,
-                        time_semantic: '4:15pm'
-                    }, {
-                        name: 'Qualification Round 3',
-                        ref: '',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    },]
-                }]
-            }, {
-                date: 'Sat Feb 4',
-                stages: [{
-                    name: 'Indoor Stage',
-                    lineup: [{
-                        name: 'Bilderbuch',
-                        ref: 'bilderbuch',
-                        time_mult: 2,
-                        time_dur: 6,
-                        time_semantic: '1:30pm'
-                    }, {
-                        name: 'Funf Sterne Deluxe',
-                        ref: 'funf-sterne-deluxe',
-                        time_mult: 13,
-                        time_dur: 7,
-                        time_semantic: '4:15pm'
-                    }, {
-                        name: 'The Naked and Famous',
-                        ref: 'the-naked-and-famous',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    }, {
-                        name: 'Biffy Clyro',
-                        ref: 'biffy-clyro',
-                        time_mult: 28,
-                        time_dur: 6,
-                        time_semantic: '8pm'
-                    }]
-                }, {
-                    name: 'Main Stage',
-                    lineup: [{
-                        name: 'Wax Wreckaz',
-                        ref: 'wax-wreckaz',
-                        time_mult: 4,
-                        time_dur: 4,
-                        time_semantic: '2:15pm'
-                    }, {
-                        name: 'Famouse Deck Team',
-                        ref: 'famouse-deck-team',
-                        time_mult: 11,
-                        time_dur: 7,
-                        time_semantic: '3:45pm'
-                    }, {
-                        name: 'Lucky Chops',
-                        ref: 'lucky-chops',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    }, {
-                        name: 'Beginner',
-                        ref: 'beginner',
-                        time_mult: 32,
-                        time_dur: 8,
-                        time_semantic: '9pm'
-                    }]
-                }, {
-                    name: 'Ramp',
-                    lineup: [{
-                        name: 'Qualification Round 1',
-                        ref: '',
-                        time_mult: 4,
-                        time_dur: 6,
-                        time_semantic: '2:15pm'
-                    }, {
-                        name: 'Qualification Round 2',
-                        ref: '',
-                        time_mult: 12,
-                        time_dur: 6,
-                        time_semantic: '4:15pm'
-                    }, {
-                        name: 'Qualification Round 3',
-                        ref: '',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    },]
-                }]
-            }, {
-                date: 'Sun Feb 5',
-                stages: [{
-                    name: 'Indoor Stage',
-                    lineup: [{
-                        name: 'Bilderbuch',
-                        ref: 'bilderbuch',
-                        time_mult: 2,
-                        time_dur: 6,
-                        time_semantic: '1:30pm'
-                    }, {
-                        name: 'Funf Sterne Deluxe',
-                        ref: 'funf-sterne-deluxe',
-                        time_mult: 13,
-                        time_dur: 7,
-                        time_semantic: '4:15pm'
-                    }, {
-                        name: 'The Naked and Famous',
-                        ref: 'the-naked-and-famous',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    }, {
-                        name: 'Biffy Clyro',
-                        ref: 'biffy-clyro',
-                        time_mult: 28,
-                        time_dur: 6,
-                        time_semantic: '8pm'
-                    }]
-                }, {
-                    name: 'Main Stage',
-                    lineup: [{
-                        name: 'Wax Wreckaz',
-                        ref: 'wax-wreckaz',
-                        time_mult: 4,
-                        time_dur: 4,
-                        time_semantic: '2:15pm'
-                    }, {
-                        name: 'Famouse Deck Team',
-                        ref: 'famouse-deck-team',
-                        time_mult: 11,
-                        time_dur: 7,
-                        time_semantic: '3:45pm'
-                    }, {
-                        name: 'Lucky Chops',
-                        ref: 'lucky-chops',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    }, {
-                        name: 'Beginner',
-                        ref: 'beginner',
-                        time_mult: 32,
-                        time_dur: 8,
-                        time_semantic: '9pm'
-                    }]
-                }, {
-                    name: 'Ramp',
-                    lineup: [{
-                        name: 'Qualification Round 1',
-                        ref: '',
-                        time_mult: 4,
-                        time_dur: 6,
-                        time_semantic: '2:15pm'
-                    }, {
-                        name: 'Qualification Round 2',
-                        ref: '',
-                        time_mult: 12,
-                        time_dur: 6,
-                        time_semantic: '4:15pm'
-                    }, {
-                        name: 'Qualification Round 3',
-                        ref: '',
-                        time_mult: 20,
-                        time_dur: 6,
-                        time_semantic: '6pm'
-                    },]
-                }]
-            }]
-            cc.activeDay = cc.ScheduleContent[0]
-        }
+            cc.tour = Tours($stateParams.tour);
+            cc.ScheduleContent = cc.tour.schedule
+            cc.activeDay = cc.ScheduleContent[0];
+            cc.currentStage = cc.activeDay.stages[0]
+        };
     }
 })();
