e09a18df9a06599760a1e9581070f0e4cb63725a
Teembokeem <myungtkim@gmail.com>
2017-01-13 15:03:03 -0800

    add preloader on live

--- www/views/global-components/preloader/Component.preloader.js
+++ www/views/global-components/preloader/Component.preloader.js
@@ -10,24 +10,21 @@
             controllerAs: 'cc'
         })
 
-    preloaderCtrl.$inject = ['$log', '$window', '$stateParams', '$rootScope'];
+    preloaderCtrl.$inject = ['$log', '$window', '$stateParams', '$rootScope', '$scope'];
 
-    function preloaderCtrl($log, $window, $stateParams, $rootScope) {
+    function preloaderCtrl($log, $window, $stateParams, $rootScope, $scope) {
         var cc = this;
 
         cc.$onInit = function () {
             cc.width = 0.80 * $window.innerWidth;
-            if ($window.innerWidth > 1023) {
+            setTimeout(function () {
+                cc.setTime = true;
+                console.log("yes", cc.setTime);
+                $scope.$apply()
                 setTimeout(function () {
-                    cc.setTime = true;
-                    setTimeout(function () {
-                        cc.hide = true;
-                    }, 100);
-                }, 2800);
-
-            } else {
-                cc.hide = true;
-            }
+                    cc.hide = true;
+                }, 100);
+            }, 3000);
         }
     }
 })();
