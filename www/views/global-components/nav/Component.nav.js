55e45a996e4750ce48d9c5bdb829f4ded20c3988
pkcy2300 <pkcy2300@gmail.com>
2017-01-25 18:07:27 +0530

    Fixed: issue #36

--- www/views/global-components/nav/Component.nav.js
+++ www/views/global-components/nav/Component.nav.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
     var dir = "/views/global-components/nav/"
     angular.module('Template')
@@ -24,13 +24,10 @@
             if (cc.mobile && cc.noNav == false) cc.noNav = true;
         }
 
+        _setMenuLayout();
+        _handleWindowResize();
 
-        if ($window.innerWidth <= 600) {
-            cc.noNav = true;
-            cc.mobile = true;
-            cc.darken = false;
-        }
-        $rootScope.$on('noNav', function (e, d) {
+        $rootScope.$on('noNav', function(e, d) {
             if ($window.innerWidth <= 600) {
                 cc.mobile = true;
 
@@ -38,25 +35,26 @@
             } else {
                 cc.mobile = false;
                 // if (!$scope.$$phase) {
-                $scope.$apply(function () {
+                $scope.$apply(function() {
                     // if (!cc.mobile) {
                     cc.noNav = d;
+                    console.log('No Nav Event: ',d);
                     // }
                 });
                 // }
             }
         });
 
-        $rootScope.$on('darkenNav', function (evt, d) {
+        $rootScope.$on('darkenNav', function(evt, d) {
             cc.darken = !cc.darken;
         })
 
-        $rootScope.$on('tourStopMenuClose', function (e, d) {
+        $rootScope.$on('tourStopMenuClose', function(e, d) {
             // console.log(e, d)
             cc.venue = d.tour ? Tours(d.tour) : null
         })
 
-        cc.toggleNav = function () {
+        cc.toggleNav = function() {
             cc.noNav = !cc.noNav;
             if (cc.mobile) {
                 // console.log("this")
@@ -65,5 +63,24 @@
             }
         };
         cc.showHams = false;
+
+        function _handleWindowResize() {
+            angular.element(window).on('resize', function() {
+                $scope.$apply(function() {
+                    _setMenuLayout();
+                    console.log();
+                });
+            });
+        }
+
+        function _setMenuLayout() {
+            if ($window.innerWidth < 800) {
+                cc.noNav = true;
+                cc.mobile = true;
+                cc.darken = false;
+            } else {
+                cc.mobile = false;
+            }
+        }
     }
 })();
