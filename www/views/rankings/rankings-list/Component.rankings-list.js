534c6d42ebd71ac1138537346f308aac43eeb8c5
Teembokeem <myungtkim@gmail.com>
2017-02-21 10:22:50 -0800

    add fix for setting rankign for innsbruck adn la

--- www/views/rankings/rankings-list/Component.rankings-list.js
+++ www/views/rankings/rankings-list/Component.rankings-list.js
@@ -26,14 +26,13 @@
                 cc.rankingsListContent = cc.rankingsListContent.sort(function (a, b) {
                     return b.scores[cc.currentYear].overall - a.scores[cc.currentYear].overall
                 });
-            }
-            else {
-                if(cc.currentYear==='2017' && (param==='innsbruck'||param==='la')) return;
+            } else {
+                // if(cc.currentYear==='2017' && (param==='innsbruck'||param==='la')) return;
                 cc.rankingsListContent = cc.rankingsListContent.sort(function (a, b) {
                     return parseInt(b.scores[cc.currentYear][param].score == 'N/A' ? 0 : b.scores[cc.currentYear][param].score) - parseInt(a.scores[cc.currentYear][param].score == 'N/A' ? 0 : a.scores[cc.currentYear][param].score)
                 })
             }
-            
+
         };
 
         function chooseOption() {
